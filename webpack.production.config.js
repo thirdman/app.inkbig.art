3665ac21ebc389ac9d6ac69bd76daef6dbd0d6d5
Gareth Bedford <g@thirdman.co.nz>
2017-10-30 22:59:25 +1300

    fixed prod webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -70,7 +70,7 @@ const config = {
       },
       {
         test: /\.css$/,
-        exclude: /node_modules/,
+        // exclude: /node_modules/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: [
